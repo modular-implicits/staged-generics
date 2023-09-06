7c827e93e95be4cfbddfd368b20f901a640253cd
Patrick Reader <_@pxeger.com>
2023-09-06 10:26:37 +0100

    initial implementation

---
+++ classes.ml
@@ -0,0 +1,28 @@
+module type Product = sig
+  (* type t = a * b *)
+  type a
+  type b
+  type t
+  val construct : a code -> b code -> t code
+  val deconstruct : t code -> a code * b code
+end
+
+module type Sum = sig
+  (* type t = a | b *)
+  type a
+  type b
+  type t
+  val construct_a : a code -> t code
+  val construct_b : b code -> t code
+  val match_ :
+    (a code -> 'r code) ->
+    (b code -> 'r code) ->
+    t code -> 'r code
+end
+
+module type Wrapper = sig
+  type t
+  type inner
+  val wrap : inner code -> t code
+  val unwrap : t code -> inner code
+end
---
+++ instances.ml
@@ -0,0 +1,51 @@
+open Imp.Any
+open Classes
+
+implicit module Pair {A: Any} {B: Any}
+:  Product
+  with type t = A.t * B.t
+  and type a = A.t
+  and type b = B.t
+= struct
+  type t = A.t * B.t
+  type a = A.t
+  type b = B.t
+  let construct a b = .< .~a, .~b >.
+  let deconstruct p = .< fst .~p >., .< snd .~p >.
+end
+
+implicit module Option {A: Any}
+: Sum
+  with type t = A.t option
+  and type a = A.t
+  and type b = unit
+= struct
+  type t = A.t option
+  type a = A.t
+  type b = unit
+  let construct_a a = .< Some .~a >.
+  let construct_b _ = .< None >.
+  let match_ fa fb x = .<
+    match .~x with
+    | Some a -> .~(fa .< a >.)
+    | None -> .~(fb .< () >.)
+  >.
+end
+
+implicit module List {A: Any}
+: Sum
+  with type t = A.t list
+  and type a = unit
+  and type b = A.t * A.t list
+= struct
+  type t = A.t list
+  type a = unit
+  type b = A.t * A.t list
+  let construct_a _ = .< [] >.
+  let construct_b p = .< let (x, xs) = .~p in x :: xs >.
+  let match_ fa fb (x: A.t list code) = .<
+    match .~x with
+    | [] -> .~(fa .< () >.)
+    | x :: xs -> .~(fb .< (x, xs) >.)
+  >.
+end
